c582723845b67158ebf87c4046733ac007bfd47d
Maryam Fatima <maryamaifatima@gmail.com>
2026-07-02 18:00:32 +0500

    Initial project setup

---
+++ server/app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const connectDB = require("./config/db");
+
+const app = express();
+
+// Database Connect
+connectDB();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Test Route
+app.get("/", (req, res) => {
+  res.send("Job Application Tracker API is Running 🚀");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
